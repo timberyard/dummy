149f45fffffce182ba278de62f7de270e7764f0c
Niklas Hofmann <niklas.hofmann@everbase.net>
2015-01-14 12:44:59 +0100

    add more unused variables

--- src/dummy.cpp
+++ src/dummy.cpp
@@ -6,6 +6,7 @@ int main(int argc, const char* const* argv)
 	float x = 5.5;
 	int y = x;
 	int z = x;
+	int a = 2;
 	std::cout << "dummy process - " << y << std::endl;
 	std::cerr << "dummy process" << std::endl;
 	return 0;
